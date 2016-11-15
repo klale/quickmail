118e7a1b092e242e355ad9107dd5d08e84559cef
Carl Persson <carl.persson@mac.com>
2016-11-15 21:27:19 +0100

    Clean up api and update readme. Added missing Date header.

--- README.txt
+++ README.txt
@@ -1,17 +1,122 @@
+# Quickmail
 
-Quickmail
------------------------------------------------
-Simple package for sending a multipart email:
-Supports 
-- plaintext emails
-- Html emails with plaintext alternative
-- Attachments
-- Embedded images in html mails
+Package for creating and sending multipart emails in python 2.7.
+
+### Features
+ - Send plain text emails
+ - Send HTML emails
+ - Add attachments
+ - Inline all rules of referenced stylesheets
+ - Embed images and backgrounds
+
+### Dependencies
+For HTML and stylesheet processing:
+ - tinycss
+ - pyquery
+
+### Example: Send a plain text email
+
+```python
+from quickmail import Mail, Connection
+
+mail = Mail(fr='sender@foo.com',
+            to='recipient@foo.com',
+            subject='Hello',
+            text='This is a plain text email',
+            attachments=['path/to/file.zip'])
+
+with Connection('smtp.foo.com', 'user', 'pass') as conn:
+    conn.send(mail)
+```
+
+Or use the shorthand
+
+```python
+with Connection('smtp.foo.com', 'user', 'pass') as conn:
+    conn.send(fr='sender@foo.com',
+              to='recipient@foo.com',
+              subject='Hello',
+              text='This is a plain text email'))
+```
+
+
+### Example: Send an HTML email
+
+This example has 4 files:
+
+ - email/email.html
+ - email/styles.css
+ - email/logo.png
+ - email/send_mail.py
+
+```html
+<!-- email.html -->
+<html>
+    <head>
+        <link rel="stylesheet" href="email.css">
+    </head>
+    </body>
+        <div class="logo">
+            <img src="logo.png">
+        </div>
+
+        <div class="section">
+            <h2>I am header</h2>
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit</p>
+        </div>
+    </body>
+</html>
+```
+```css
+/* styles.css */
+.logo {
+    border: 1px solid #999;
+}
+p {
+  margin: 20px 0;
+}
+```
+
+```python
+# send_mail.py
+
+from quickmail import Mail, Connection
+
+with file('email/email.html') as f:
+    html = f.read()
+
+# Create a new HTMLDoument
+doc = HTMLDocument(html,
+               # Prepend to relative paths
+               base_url='https://www.acme.com/',
+               pub_dir=os.path.dirname(__file__))
+
+
+# Optionally inline styles and embed images and backgrounds
+doc.embed_images()
+doc.embed_backgrounds()
+doc.inline_styles()
+# >>> doc.html
+# '<html>.......<div style="border: 1px solid #999;"><img src="cid:logo.png".......'
+# >>> doc.images
+# {'logo.png': 'path/to/logo.png'}
+
+# Send it
+with Connection('smtp.foo.com', 'user', 'pass') as conn:
+    conn.send(fr='sender@foo.com',
+              to='recipient@foo.com',
+              subject='Hello',
+              text='Plain text version',
+              html=doc.html,
+              images=doc.images))
+
+```
 
 
 
-Using embedded images in html mail
-------------------------------------
-cid is short for "Content-ID". See __init__.py
 
-<img src="cid:t.gif">
+### Todo
+
+- Add tests
+- Python 3 support
+- More documentation
--- quickmail/__init__.py
+++ quickmail/__init__.py
@@ -7,41 +7,22 @@ from email.mime.image import MIMEImage
 from email.mime.base import MIMEBase
 from email import Encoders
 import logging
+import email
 
 from quickmail.error import MailException
 
 log = logging.getLogger(__name__)
 
 
+
+
 # =========
 # = Utils =
 # =========
-def embed():
-    pass
 
 def guess_mime_tuple(filename, default='application/octet-stream'):
     mimetype, encoding = mimetypes.guess_type(filename or '')
     return (mimetype or default).split('/')
-
-class readfile(object):
-    
-    def __init__(self, src):
-        """
-        src can be a path to a file, or anything filelike 
-        implementing .read() and .close()
-        """
-        self.src = src
-        self.fp = None
-    
-    def __enter__(self):
-        if hasattr(self.src, 'read'):
-            self.fp = self.src
-        else:
-            self.fp = open(self.src, 'rb')
-        return self.fp
-        
-    def __exit__(self):
-        self.fp.close()
 
 
 # ==========
@@ -50,35 +31,21 @@ class readfile(object):
 class Mail(object):
     """
     Send a simple text and/or html e-mail with optional attachments
+
+
+    Example
+    -------
+    mail = Mail(fr='sender@foo.com',
+                to='recipient@foo.com',
+                subject='Hello',
+                text='Hello there, this is a plain text email')
+
+    with Connection('smtp.foo.com', 'user', 'pass') as conn:
+        conn.send(mail)
+
     """
 
-    @classmethod
-    def connect(cls, host, user=None, password=None):
-        smtp = smtplib.SMTP()
-        smtp.connect(host)
-        smtp.ehlo()
-        try:
-            smtp.starttls()        
-        except smtplib.SMTPException, e:
-            # "STARTTLS extension not supported by server"
-            pass 
-            
-        if user:
-            try: 
-                smtp.login(user, password)
-            except smtplib.SMTPException, e:
-                # this occurres when authenticting an already 
-                # pop-before-smtp authenticated session
-                pass
-        return smtp
 
-
-    
-    @classmethod
-    def disconnect(cls, smtpConnection):
-        smtpConnection.quit()
-    
-    
     def __init__(self, fr, to, subject, **kw):
         """
         Init Mail.
@@ -89,18 +56,18 @@ class Mail(object):
         kw[text] = Unicode
         kw[encoding] = String, encoding of message, default is "utf-8"
         kw[returnPath] = Unicode, email address
-        kw[attachments] = Files to attach to message. List of filelike, path or 2- or 3 tuples, or 
+        kw[attachments] = Files to attach to message. List of filelike, path or 2- or 3 tuples, or
             any combination of these. Examples:
             attachments=['/path/to/file/']
             attachments=[filelike, '/path/to/second_file.jpg']
             attachments=[('attached_file_name.jpg', filelike)]
             attachments=[('attached_file_name.jpg', filelike, 'image/jpeg')]
-            
-        kw[images] = List of (id, image) tuples (or the equivalent dict) to embed in the email 
+
+        kw[images] = List of (id, image) tuples (or the equivalent dict) to embed in the email
                      for use in a html message.
                      Example: [('myphoto', 'path/to/photo.jpg')] <img src="cid:myphoto">
         kw[headers] = Dict of additional headers, eg: {headerName: "headerValue"}
-        
+
         A Mail instance should have one or more of kw[html] or kw[text] specified.
         """
         self.fr = fr
@@ -113,34 +80,32 @@ class Mail(object):
         self.images = kw.get('images', None)
         self.returnPath = kw.get('returnPath', None)
         self.headers = kw.get('headers', {})
-        
+
         if isinstance(self.images, dict):
             self.images = self.images.items()
-        
+
         # encode any given unicodes to self.encoding (python's mail module doesn't do any encoding)
         for a in ['html', 'text']:
             v = getattr(self, a)
             if v:
                 setattr(self, a, v.encode(self.encoding, 'xmlcharrefreplace'))
-        
+
 
     def isMultipart(self):
         return self.html or self.attachments or self.images
 
 
-
-    def send(self, smtpConnection=None):
-        
+    def build(self):
         # Create the root message
         if self.isMultipart():
             # Todo: support both "related" and "mixed" parts in the same mail.
-            # "related" is used to (to some degree) hide the embedded images 
+            # "related" is used to (to some degree) hide the embedded images
             # of a html mail. "mixed" is the classic arbitrary attachement mode
             # normally used by this module
             multipart_type = 'related' if self.images else 'mixed'
-            msgRoot = MIMEMultipart(multipart_type) 
+            msgRoot = MIMEMultipart(multipart_type)
             msgRoot.preamble = 'This is a multi-part message in MIME format.'
-            
+
 
             # Encapsulate the plain and HTML versions of the message body in an
             # 'alternative' part, so message agents can decide which they want to display.
@@ -150,16 +115,16 @@ class Mail(object):
             else:
                 msgBody = msgRoot
 
-        
+
             if self.text:
-                msgText = MIMEText(self.text, 'plain', self.encoding) 
+                msgText = MIMEText(self.text, 'plain', self.encoding)
                 msgBody.attach(msgText)
-        
+
             if self.html:
                 msgHtml = MIMEText(self.html, 'html', self.encoding)
                 msgBody.attach(msgHtml)
 
-                
+
             # Arbitrary attachments of any file type
             if self.attachments:
                 for f in self.attachments:
@@ -172,68 +137,62 @@ class Mail(object):
                         else:
                             filename, f = f
                     else:
-                        filename = None    
+                        filename = None
 
                     if hasattr(f, 'read'):
                         # File-like
                         if not filename:
                             filename = getattr(f, 'filename', None)
                         part = MIMEBase(*(mime_tuple or guess_mime_tuple(filename)))
-                        
+
                         # Ensure pointer is at beginning of file
                         f.seek(0)
                         part.set_payload(f.read())
-                        
+
                     elif isinstance(f, basestring):
                         # File path
                         if not filename:
-                            filename = os.path.basename(f)                        
+                            filename = os.path.basename(f)
                         part = MIMEBase(*(mime_tuple or guess_mime_tuple(filename)))
-                        with open(f,'rb') as fp:
+                        with open(f, 'rb') as fp:
                             part.set_payload(fp.read())
                     else:
                         raise MailException('Invalid attachment: %s' % f)
 
                     if not filename:
                         filename = 'no_file_name'
-                    
+
                     Encoders.encode_base64(part)
                     # Content-Disposition can also be set to inline, then attached files wont show up
-                    # in list over attached files, but can be used in mail message, eg embedded images. 
+                    # in list over attached files, but can be used in mail message, eg embedded images.
                     # See source of an apple mail created with a stationary.
                     part.add_header('Content-Disposition', 'attachment; filename="%s"' % filename)
                     part.add_header('Content-ID', "<%s>" % filename)
                     msgRoot.attach(part)
 
-            # Embedded images for use in html mail                    
+            # Embedded images for use in html mail
             if self.images:
                 for cid, img in self.images:
                     # Create part
-                    if img.__class__.__name__ == 'Attachment': # Todo: remove ACM related from this module
-                        img.seek(0)
-                        part = MIMEImage(img.read())
-                    else:
-                        fp = open(img, 'rb')
+                    with open(img, 'rb') as fp:
                         part = MIMEImage(fp.read())
-                        fp.close()
 
                     # UPDATE: setting inline made the images appear as attachments
-                    # Removing the header fixed it. Don't know why I added it in the first place
-                    # part.add_header('Content-Disposition', 'inline; filename=%s' % cid)                        
+                    # Removing the header fixed it.
+                    # part.add_header('Content-Disposition', 'inline; filename=%s' % cid)
                     part.add_header('Content-ID', "<%s>" % cid)
                     msgRoot.attach(part)
-                    
-                    
 
         else:
             # non-multipart message
             msgRoot = MIMEText(self.text, 'plain', self.encoding)
-                        
+
 
         # fill in the from, to, and subject headers
         msgRoot['Subject'] = self.subject
         msgRoot['From'] = self.fr
-        
+        msgRoot['Date'] = email.utils.formatdate(localtime=True)
+
         # msgRoot['To'] must be a string
         # see: http://mail.python.org/pipermail/python-list/2006-March/374854.html
         to = ', '.join(self.to) if isinstance(self.to, list) else self.to
@@ -242,80 +201,74 @@ class Mail(object):
         # add optional return-path (where bounced mail goes)
         if self.returnPath:
             msgRoot.add_header('Return-path', self.returnPath)
-        
+
         # add any extra headers
         for key, val in self.headers.iteritems():
             msgRoot.add_header(key, val)
 
-        # Send the email (this example assumes SMTP authentication is required)
-        try: 
-            if smtpConnection:
-                # send through already existing connection
-                smtpConnection.sendmail(self.fr, self.to, msgRoot.as_string())
-            else:
-                smtpConnection = Mail.connect()
-                smtpConnection.sendmail(self.fr, self.to, msgRoot.as_string())
-                Mail.disconnect(smtpConnection)
-        except UnicodeEncodeError, e:
-            # occurres when self.fr or self.to containes illegal characters
-            raise MailException('Invalid sender or recipient, unicode error.')
-        except smtplib.SMTPRecipientsRefused, e:
-            raise MailException('Invalid recipient')
-        except smtplib.SMTPSenderRefused, e:
-            raise MailException('Invalid sender')
-        #except smtplib.SMTPServerDisconnected, e:
-        #    pass
+
+        return msgRoot.as_string()
 
 
-class QuickMail(object):
-    """
-    Configured maining object.
-    quickmail = QuickMail('localhost:587', 'username', '12345')
-    with quickmail.connection() as conn:
-        for email in emails:
-            conn.send(fr='a@b.com', to=email, subject='Test', text='Bla bla')
 
-    After the iteration, the single connection is implictly closed.
-    
-    
-    """
-    def __init__(self, server, username=None, password=None):
-        self.server = server
-        self.username = username
-        self.password = password
-        
-    def send(self, *args, **kw):
-        with self.connection() as conn:
-            conn.send(*args, **kw)    
-    
-    def connection(self):
-        return QuickMailConnection(self.server, self.username, self.password)
-        
-    
-        
-class QuickMailConnection(object):
-    
+class Connection(object):
+
     def __init__(self, server, username, password):
         self.server = server
         self.username = username
-        self.password = password        
+        self.password = password
 
     def __enter__(self):
-        self.connection = Mail.connect(self.server, self.username, self.password)
+        self.connect()
         return self
-    
+
     def __exit__(self, type, value, traceback):
-        self.connection.close()
-    
+        self.close()
+
+
+    def connect(self):
+        self.smtp = smtplib.SMTP()
+        self.smtp.connect(self.server)
+        self.smtp.ehlo()
+        try:
+            self.smtp.starttls()
+        except smtplib.SMTPException:
+            # "STARTTLS extension not supported by server"
+            pass
+
+        if self.username:
+            try:
+                self.smtp.login(self.username, self.password)
+            except smtplib.SMTPException:
+                # this occurres when authenticting an already
+                # pop-before-smtp authenticated session
+                pass
+
+    def close(self):
+        self.smtp.close();
+
+
     def send(self, *args, **kw):
         if args and isinstance(args[0], Mail):
             mail = args[0]
         else:
             mail = Mail(*args, **kw)
-        mail.send(self.connection)
-        
-        
-        
-        
-        
-        
+
+        # Construct the raw email message
+        email_message = mail.build()
+
+        # Send the raw email message to the smtp server
+        try:
+            self.smtp.sendmail(mail.fr, mail.to, email_message)
+        except UnicodeEncodeError:
+            # occurres when self.fr or self.to contains illegal characters
+            raise MailException('Invalid sender or recipient, unicode error.')
+        except smtplib.SMTPRecipientsRefused:
+            raise MailException('Invalid recipient')
+        except smtplib.SMTPSenderRefused:
+            raise MailException('Invalid sender')
+
+
+# Legacy
+QuickMailConnection = Connection
+
