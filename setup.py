8d6b67d67d92acaea3acd0081d66b99992ccd5f7
Carl Persson <carl.persson@mac.com>
2016-11-15 21:48:16 +0100

    Added missing "packages" to setup.py

--- setup.py
+++ setup.py
@@ -1,10 +1,11 @@
-from setuptools import setup, find_packages
+from setuptools import setup
 
 setup(
     name="Quickmail",
     version='0.1',
+    packages=['quickmail'],
     install_requires=[
         "pyquery",
-        'tinycss'        
+        'tinycss'
     ],
 )
